f49ead442488b32897b3aa00bbcc0691b2ad61b8
janghwal <jhp337037@gmail.com>
2025-05-19 16:58:02 +0900

    Feat: Store 엔티티 getter 및 생성자, 서비스 메서드 추가
    - storeStatus를 open으로 변경하는 서비스 메서드 추가(setter)

--- src/main/java/com/example/clothsdanawa/store/entity/Store.java
+++ src/main/java/com/example/clothsdanawa/store/entity/Store.java
@@ -18,12 +18,14 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Stores")
 @DynamicInsert
 @NoArgsConstructor
+@Getter
 public class Store extends Timestamped {
 	/*
 	  컬럼 - 연관관계 컬럼을 제외한 컬럼을 정의합니다.
@@ -40,6 +42,12 @@ public class Store extends Timestamped {
 	/*
 	  생성자 - 약속된 형태로만 생성가능하도록 합니다.
 	 */
+	public Store(StoreCreateRequestDto requestDto) {
+		this.company = requestDto.getCompany();
+		this.storeStatus = StoreStatus.PENDING;
+		this.storeNumber = requestDto.getStoreNumber();
+		this.address = requestDto.getAddress();
+	}
 
 	/*
 	  연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
@@ -57,4 +65,7 @@ public class Store extends Timestamped {
 	/*
 	  서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
 	 */
+	public void approveStore() {
+		this.storeStatus = StoreStatus.OPEN;
+	}
 }
