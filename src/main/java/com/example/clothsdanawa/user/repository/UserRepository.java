16cf3e01df885c78297da636153de2c9192bb471
obt <byeongtaek12@naver.com>
2025-05-19 17:27:09 +0900

    refactor: UserRepository인터페이스) 코드 위치 변경
    가독성을 위해서

--- src/main/java/com/example/clothsdanawa/user/repository/UserRepository.java
+++ src/main/java/com/example/clothsdanawa/user/repository/UserRepository.java
@@ -9,6 +9,7 @@ import com.example.clothsdanawa.user.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByEmail(String email);
+	boolean existsByEmail(String email);
 
 	default User findByIdOrElseThrow(Long userId) {
 		return findById(userId).orElseThrow(()-> new NullPointerException("사용자가 존재하지 않습니다."));
