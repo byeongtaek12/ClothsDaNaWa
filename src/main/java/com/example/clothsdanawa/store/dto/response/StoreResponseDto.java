f9679f17c2cfd4087440e4cdc34239f48b53e22c
janghwal <jhp337037@gmail.com>
2025-05-19 16:52:24 +0900

    Feat: 스토어 관련 요청 및 응답 DTO 3종 추가
    - 스토어 생성 시 사용하는 요청 DTO 구현
    - 스토어 검색 시 페이지 정보, 검색어를 담는 DTO 구현
    - 스토어 상세 정보 응답용 DTO 작성

---
+++ src/main/java/com/example/clothsdanawa/store/dto/response/StoreResponseDto.java
@@ -0,0 +1,24 @@
+package com.example.clothsdanawa.store.dto.response;
+
+import com.example.clothsdanawa.store.common.StoreStatus;
+import com.example.clothsdanawa.store.entity.Store;
+
+public class StoreResponseDto {
+	private Long storeId;
+	private String company;
+	private StoreStatus storeStatus;
+	private String storeNumber;
+	private String address;
+	private Long userId;
+	private String name;
+
+	public StoreResponseDto(Store store) {
+		this.storeId = store.getStoreId();
+		this.company = store.getCompany();
+		this.storeStatus = store.getStoreStatus();
+		this.storeNumber = store.getStoreNumber();
+		this.address = store.getStoreNumber();
+		this.userId = store.getUser().getUserId();
+		this.name = store.getUser().getName();
+	}
+}
