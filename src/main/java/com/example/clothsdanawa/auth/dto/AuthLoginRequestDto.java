aa21a6c7c5dac81dac65191f8cfdf43a2c9f603a
obt <byeongtaek12@naver.com>
2025-05-19 19:53:06 +0900

    refactor: AuthLoginRequest) 변수타입 수정
    Long으로 잘못 적어놔서 String으로 수정

--- src/main/java/com/example/clothsdanawa/auth/dto/AuthLoginRequestDto.java
+++ src/main/java/com/example/clothsdanawa/auth/dto/AuthLoginRequestDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class AuthLoginRequestDto {
-	private Long email;
-	private Long password;
+	private String email;
+	private String password;
 
 }
