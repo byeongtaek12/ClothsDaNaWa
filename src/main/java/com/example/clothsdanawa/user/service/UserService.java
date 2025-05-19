c52102fe09536ecb5732fd5a601936e884163625
obt <byeongtaek12@naver.com>
2025-05-19 14:57:30 +0900

    feat: UserService클래스) 생성 및 유저 조회 메서드 추가
    ``

--- src/main/java/com/example/clothsdanawa/user/service/UserService.java
+++ src/main/java/com/example/clothsdanawa/user/service/UserService.java
@@ -1,4 +1,21 @@
 package com.example.clothsdanawa.user.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.clothsdanawa.user.dto.UserResponseDto;
+import com.example.clothsdanawa.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+	private final UserRepository userRepository;
+
+	public UserResponseDto getUserById(Long userId) {
+
+		return UserResponseDto.from(userRepository.findByIdOrElseThrow(userId));
+
+	}
 }
