1080b09bf4bab7fd64a946aed5dc27eaa8d58f91
obt <byeongtaek12@naver.com>
2025-05-19 14:56:35 +0900

    feat: UserController클래스) 생성 및 유저 조회 메서드 추가
    ``

--- src/main/java/com/example/clothsdanawa/user/controller/UserController.java
+++ src/main/java/com/example/clothsdanawa/user/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.example.clothsdanawa.user.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.clothsdanawa.user.dto.UserResponseDto;
+import com.example.clothsdanawa.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+	private final UserService userService;
+
+	@GetMapping("/{user_id}")
+	public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long user_id) {
+
+		return ResponseEntity.status(200).body(userService.getUserById(user_id));
+
+	}
+
+
 }
