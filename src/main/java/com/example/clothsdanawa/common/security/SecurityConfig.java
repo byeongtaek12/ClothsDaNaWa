5e530a2114070fdcf5d8dbba9b6c0345a8d4bc9a
obt <byeongtaek12@naver.com>
2025-05-19 17:24:27 +0900

    feat: SecurityConfig클래스) 생성
    ``

---
+++ src/main/java/com/example/clothsdanawa/common/security/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.example.clothsdanawa.common.security;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.example.clothsdanawa.common.jwt.JwtFilter;
+import com.example.clothsdanawa.common.jwt.JwtUtil;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final JwtUtil jwtUtil;
+	private final CustomUserDetailsService userDetailsService;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable) // csrf 기능 비활성화
+			.sessionManagement(sm ->
+				sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/auth/**").permitAll()  // WHITE_LIST 느낌
+				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.requestMatchers("/stores/**").hasRole("OWNER")
+				.anyRequest().authenticated()) // 나머지는 인증 필요
+			.anonymous(AbstractHttpConfigurer::disable) // 익명 객체 생성 비활성화
+			.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+			.exceptionHandling((ex-> ex
+				.authenticationEntryPoint(((request,
+					response,
+					authException) -> response.sendError(401)))))
+		;
+
+		return http.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	public Filter jwtFilter() {
+		return new JwtFilter(jwtUtil, userDetailsService);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
+
