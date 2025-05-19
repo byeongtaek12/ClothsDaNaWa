323d910ebb949c912f6f302bfab0daf64edf6930
obt <byeongtaek12@naver.com>
2025-05-19 12:02:02 +0900

    feat: BaseEntity 생성 및 application.properties 추가
    ``

--- src/main/java/com/example/clothsdanawa/ClothsDaNaWaApplication.java
+++ src/main/java/com/example/clothsdanawa/ClothsDaNaWaApplication.java
@@ -2,8 +2,10 @@ package com.example.clothsdanawa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ClothsDaNaWaApplication {
 
 	public static void main(String[] args) {
