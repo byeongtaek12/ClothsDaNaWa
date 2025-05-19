b183129e066fd907359efe30bdf2b6c06b4f7a82
obt <byeongtaek12@naver.com>
2025-05-19 14:57:15 +0900

    feat: UserRepository인터페이스) 생성 및 유저 조회 메서드 추가
    ``

--- src/main/java/com/example/clothsdanawa/user/repository/UserRepository.java
+++ src/main/java/com/example/clothsdanawa/user/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.example.clothsdanawa.user.repository;
 
-public interface UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.clothsdanawa.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	default User findByIdOrElseThrow(Long userId) {
+		return findById(userId).orElseThrow(()-> new NullPointerException("사용자가 존재하지 않습니다"));
+	}
 }
