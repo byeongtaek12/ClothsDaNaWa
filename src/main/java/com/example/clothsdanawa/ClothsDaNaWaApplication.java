cf9bfe193aa0630a9b5ab2721a6376dab584f6d7
obt <byeongtaek12@naver.com>
2025-05-19 10:09:33 +0900

    init

---
+++ src/main/java/com/example/clothsdanawa/ClothsDaNaWaApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothsdanawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothsDaNaWaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothsDaNaWaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clothsdanawa/ClothsDaNaWaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothsdanawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothsDaNaWaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
