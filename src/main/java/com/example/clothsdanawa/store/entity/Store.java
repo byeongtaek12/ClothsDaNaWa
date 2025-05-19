2d20ea9d79780ba558fad94f7d142e73b71948f6
janghwal <jhp337037@gmail.com>
2025-05-19 16:46:09 +0900

    Refactor: 연관 관계의 FetchType을 LAZY로 설정

--- src/main/java/com/example/clothsdanawa/store/entity/Store.java
+++ src/main/java/com/example/clothsdanawa/store/entity/Store.java
@@ -10,6 +10,7 @@ import com.example.clothsdanawa.store.dto.request.StoreCreateRequestDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -43,16 +44,15 @@ public class Store extends Timestamped {
 	/*
 	  연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
 	 */
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	@OneToMany(mappedBy = "products")
+	@OneToMany(mappedBy = "products", fetch = FetchType.LAZY)
 	private List<Product> product;
 	/*
 	  연관관계 편의 메소드 - 반대쪽에는 연관관계 편의 메소드가 없도록 주의합니다.
 	 */
-
 
 	/*
 	  서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
