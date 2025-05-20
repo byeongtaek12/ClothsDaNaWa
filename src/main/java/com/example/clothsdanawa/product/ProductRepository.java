a66ba982943a39076c154522f3c62caec686ed7f
obt <byeongtaek12@naver.com>
2025-05-20 17:30:48 +0900

    chore: 파일 정리
    ``

--- src/main/java/com/example/clothsdanawa/product/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.clothsdanawa.product;
-
-import com.example.clothsdanawa.product.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
--- src/main/java/com/example/clothsdanawa/product/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.clothsdanawa.product.repository;
-
-import com.example.clothsdanawa.product.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
