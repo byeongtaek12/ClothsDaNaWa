fadcd111a6ff95cbc6692eac434d239e42623515
bella <yiseuljeong0415@gmail.com>
2025-05-20 11:47:31 +0900

    feat(product): 상품 등록/수정/재고 요청,응답 DTO 클래스 생성

---
+++ src/main/java/com/example/clothsdanawa/product/dto/request/ProductStockRequest.java
@@ -0,0 +1,12 @@
+package com.example.clothsdanawa.product.dto.request;
+
+import lombok.Getter;
+
+/**
+ * 재고 차감 요청 DTO
+ */
+@Getter
+public class ProductStockRequest {
+
+	private int quantity;
+}
