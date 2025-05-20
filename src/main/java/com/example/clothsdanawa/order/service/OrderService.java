cce63a59dd5f40b9958d76e34d910b42db981723
romin014 <sohyun014@naver.com>
2025-05-20 12:00:20 +0900

    docs : 주석

--- src/main/java/com/example/clothsdanawa/order/service/OrderService.java
+++ src/main/java/com/example/clothsdanawa/order/service/OrderService.java
@@ -49,7 +49,6 @@ public class OrderService {
 			//.cart(cart)
 			.build();
 
-
 		Order savedOrder = orderRepository.save(order);
 
 		return new OrderResponseDto(
@@ -66,66 +65,64 @@ public class OrderService {
 	}
 
 	// todo : 2. 주문 조회
-	public OrderResponseDto findOrder(Long id){
-		Order order = orderRepository.findById(id);//.orElseThrow(() ->
-			//new InvalidRequestException("Order not found"));
-
-		// Cart cart = order.getCart();
-
-		// List<Cart> cartItems = cart.getCartItem();
-
-		return new OrderResponseDto(
-			order.getId(),
-			//order.getCartId(),
-			order.getQuantity(),
-			// order.getCartList(),
-			// order.getTotalPrice
-			order.getPoint(),
-			order.getCreatedAt(),
-			order.getOrderStatus()
-			//cartItems
-		);
-	}
-
-	// todo : 3. 주문 상태 변경
-	public String updateStatus(Long id, OrderStatus newStatus) {
-		Order order = orderRepository.findById(id);
-			//.orElseThrow(() -> new InvalidRequestException("Order not found"));
-
-		//order.setOrderStatus(newStatus);
-		orderRepository.save(order);
-
-		// 메시지 정의
-		String message;
-		switch (newStatus) {
-			case IN_DELIVERY:
-				message = "배송 중입니다.";
-				break;
-			case COMPLETED:
-				message = "배송이 완료되었습니다.";
-				break;
-			default:
-				message = "상태가 업데이트되었습니다.";
-		}
-
-		return message;
-	}
-
-	// todo : 4. 주문 요청 취소
-	public String cancelOrder(Long orderId) {
-		Order order = orderRepository.findById(orderId);
-			//.orElseThrow(() -> new InvalidRequestException("Order not found"));
-
-		// if (order.getOrderStatus() == OrderStatus.CANCELLED) {throw new 예외처리("이미 취소된 주문입니다.");
-
-		// if (order.getOrderStatus() == OrderStatus.COMPLETED) {throw new 예외처리("배송 완료된 주문은 취소할 수 없습니다.");
-
-		order.setOrderStatus(OrderStatus.CANCELLED);
-		orderRepository.save(order);
-
-		return "주문이 취소되었습니다.";
-	}
-
-
+	// public OrderResponseDto findOrder(Long id){
+	// 	Order order = orderRepository.findById(id);//.orElseThrow(() ->
+	// 		//new InvalidRequestException("Order not found"));
+	//
+	// 	// Cart cart = order.getCart();
+	//
+	// 	// List<Cart> cartItems = cart.getCartItem();
+	//
+	// 	return new OrderResponseDto(
+	// 		order.getId(),
+	// 		//order.getCartId(),
+	// 		order.getQuantity(),
+	// 		// order.getCartList(),
+	// 		// order.getTotalPrice
+	// 		order.getPoint(),
+	// 		order.getCreatedAt(),
+	// 		order.getOrderStatus()
+	// 		//cartItems
+	// 	);
+	// }
+	//
+	// // todo : 3. 주문 상태 변경
+	// public String updateStatus(Long id, OrderStatus newStatus) {
+	// 	Order order = orderRepository.findById(id);
+	// 		//.orElseThrow(() -> new InvalidRequestException("Order not found"));
+	//
+	// 	//order.setOrderStatus(newStatus);
+	// 	orderRepository.save(order);
+	//
+	// 	// 메시지 정의
+	// 	String message;
+	// 	switch (newStatus) {
+	// 		case IN_DELIVERY:
+	// 			message = "배송 중입니다.";
+	// 			break;
+	// 		case COMPLETED:
+	// 			message = "배송이 완료되었습니다.";
+	// 			break;
+	// 		default:
+	// 			message = "상태가 업데이트되었습니다.";
+	// 	}
+	//
+	// 	return message;
+	// }
+	//
+	// // todo : 4. 주문 요청 취소
+	// public String cancelOrder(Long orderId) {
+	// 	Order order = orderRepository.findById(orderId);
+	// 		//.orElseThrow(() -> new InvalidRequestException("Order not found"));
+	//
+	// 	// if (order.getOrderStatus() == OrderStatus.CANCELLED) {throw new 예외처리("이미 취소된 주문입니다.");
+	//
+	// 	// if (order.getOrderStatus() == OrderStatus.COMPLETED) {throw new 예외처리("배송 완료된 주문은 취소할 수 없습니다.");
+	//
+	// 	order.setOrderStatus(OrderStatus.CANCELLED);
+	// 	orderRepository.save(order);
+	//
+	// 	return "주문이 취소되었습니다.";
+	// }
 
 }
