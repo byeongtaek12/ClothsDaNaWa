586c2f4b2ba1415f9e3add9196476eb297514f37
류형철 <fbgudcjf2@gmail.com>
2025-05-21 12:15:09 +0900

    feat: Cart 에러코드 추가,반영

--- src/main/java/com/example/clothsdanawa/cart/service/CartService.java
+++ src/main/java/com/example/clothsdanawa/cart/service/CartService.java
@@ -6,16 +6,17 @@ import com.example.clothsdanawa.cart.entity.Cart;
 import com.example.clothsdanawa.cart.entity.CartItem;
 import com.example.clothsdanawa.cart.repository.CartItemRepository;
 import com.example.clothsdanawa.cart.repository.CartRepository;
+import com.example.clothsdanawa.common.exception.BaseException;
+import com.example.clothsdanawa.common.exception.ErrorCode;
 import com.example.clothsdanawa.product.entity.Product;
 import com.example.clothsdanawa.product.repository.ProductRepository;
 import com.example.clothsdanawa.store.common.StoreStatus;
 import com.example.clothsdanawa.user.entity.User;
 import com.example.clothsdanawa.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
+
 import java.util.Optional;
 
 @Service
@@ -33,7 +34,7 @@ public class CartService {
     public CartResponseDto createCart(Long userId, Long productId, int quantity) {
         //product 조회
         Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 제품을 찾을 수 없습니다."));
+                .orElseThrow(() -> new BaseException(ErrorCode.CART_NOT_FOUND));
 
         User user = userRepository.findById(userId).get();
 
@@ -41,7 +42,7 @@ public class CartService {
                 .orElseGet(() -> cartRepository.save(Cart.of(user)));
         //status OPEN 상태인 쇼핑몰만
         if (!product.getStore().getStoreStatus().equals(StoreStatus.OPEN)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "운영중인 쇼핑몰이 아닙니다.");
+            throw new BaseException(ErrorCode.CART_STORE_CLOSED);
         }
         //장바구니 만료시간조회 만료시 장바구니비우기
         if (cart.isExpired()) {
@@ -58,7 +59,7 @@ public class CartService {
 
         //장바구니 수량이 stock 수량보다 많은지 검증
         if (product.getStock() < totalQty) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "요청 수량이 재고보다 많습니다.");
+            throw new BaseException(ErrorCode.CART_QUANTITY_EXCEEDED);
         }
         //있으면 수량추가
         if (cartItem.isPresent()) {
@@ -80,10 +81,10 @@ public class CartService {
     public CartResponseDto findMyCart(Long userId) {
 
         Cart cart = cartRepository.findByUserId(userId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "장바구니가 없습니다."));
+                .orElseThrow(() -> new BaseException(ErrorCode.CART_NOT_FOUND));
 
         if (cart.getCartItems().isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NO_CONTENT, "장바구니가 비어있습니다.");
+            throw new BaseException(ErrorCode.CART_EMPTY);
         }
         cart.calculateTotalPrice();
 
@@ -94,16 +95,16 @@ public class CartService {
     @Transactional
     public CartResponseDto updateCart(Long userId, Long cartItemId, int quantity) {
         Cart cart = cartRepository.findByUserId(userId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "장바구니가 없습니다."));
+                .orElseThrow(() -> new BaseException(ErrorCode.CART_NOT_FOUND));
         //장바구니에 cartItemId 상품이 있는지 찾기
         CartItem cartItem = cart.getCartItems().stream()
                 .filter(item -> item.getId().equals(cartItemId)).findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "장바구니에 해당 상품이 없습니다."));
+                .orElseThrow(() -> new BaseException(ErrorCode.CART_ITEM_NOT_FOUND));
         //해당상품의 재고
         int stock = cartItem.getProduct().getStock();
         //재고수량이 부족할때
         if (stock < quantity) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "재고가 부족합니다.");
+            throw new BaseException(ErrorCode.CART_QUANTITY_EXCEEDED);
         }
         //0으로 수정할때 삭제
         cartItem.updateQuantity(cartItem, quantity);
