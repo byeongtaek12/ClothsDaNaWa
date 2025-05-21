6176c44f4334b26f7e73bffe6cbed226bac377e1
obt <byeongtaek12@naver.com>
2025-05-21 17:54:23 +0900

    refactor: of 메서드 수정
    - 이메일을 변경하고 다시 재로그인을 하지 않고 서비스 이용시 401에러가 반환되기 때문에 추가로 설명까지 넣음

--- src/main/java/com/example/clothsdanawa/user/dto/UserUpdateResponseDto.java
+++ src/main/java/com/example/clothsdanawa/user/dto/UserUpdateResponseDto.java
@@ -23,7 +23,7 @@ public class UserUpdateResponseDto {
 		return UserUpdateResponseDto.builder()
 			.id(user.getUserId())
 			.name(user.getName())
-			.email(user.getEmail())
+			.email("이메일을 " + user.getEmail() + " 로 수정완료하였습니다. 재로그인시 서비스이용가능합니다")
 			.password(requestPassword != null ? requestPassword : "변동없습니다")
 			.address(user.getAddress())
 			.userRole(user.getUserRole().toString())
