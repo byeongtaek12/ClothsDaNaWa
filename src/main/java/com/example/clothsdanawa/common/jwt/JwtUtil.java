8d5493c8f43ac3f8750f5618a30a9d0ff8fd5e78
romin014 <sohyun014@naver.com>
2025-05-21 16:07:07 +0900

    add : 삭제된 파일 복구

---
+++ src/main/java/com/example/clothsdanawa/common/jwt/JwtUtil.java
@@ -0,0 +1,65 @@
+package com.example.clothsdanawa.common.jwt;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import com.example.clothsdanawa.user.entity.UserRole;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class JwtUtil {
+
+	private static final String BEARER_PREFIX = "Bearer ";
+	private static final long TOKEN_TIME = 60 * 60 * 1000L;
+
+	@Value("${jwt.secret.key}")
+	private String secretKey;
+	private Key key;
+	private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+	@PostConstruct
+	public void init() {
+		byte[] bytes = Base64.getDecoder().decode(secretKey);
+		key = Keys.hmacShaKeyFor(bytes);
+	}
+
+	public String createToken(Long userId, String name, String email, UserRole userRole) {
+		Date date = new Date();
+
+		return BEARER_PREFIX + Jwts.builder()
+			.setSubject(String.valueOf(userId))
+			.claim("name", name)
+			.claim("email", email)
+			.claim("userRole", userRole)
+			.setExpiration(new Date(date.getTime() + TOKEN_TIME))
+			.setIssuedAt(date)
+			.signWith(key, signatureAlgorithm)
+			.compact();
+	}
+
+	public String substringToken(String tokenValue) {
+		if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+			return tokenValue.substring(7);
+		}
+		throw new NullPointerException("Not Found Token");
+	}
+
+	public Claims extractClaims(String token) {
+		return Jwts.parserBuilder()
+			.setSigningKey(key)
+			.build()
+			.parseClaimsJws(token)
+			.getBody();
+	}
+}
+
