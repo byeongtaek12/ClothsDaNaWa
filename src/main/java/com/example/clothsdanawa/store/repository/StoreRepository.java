13906fcde0772fca449d1fd6f7ec6a014561f7da
bella <yiseuljeong0415@gmail.com>
2025-05-21 10:37:19 +0900

    refactor : store 태스트 부분 삭제

--- src/main/java/com/example/clothsdanawa/store/repository/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.clothsdanawa.store.repository;
-
-import com.example.clothsdanawa.store.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// 테스트용 레포 지우셈
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
