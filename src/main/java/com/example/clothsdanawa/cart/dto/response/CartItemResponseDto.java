f3fb07a080088bec90da51602b388aae0095c441
류형철 <fbgudcjf2@gmail.com>
2025-05-21 15:13:43 +0900

    feat: 카트 CRUD , 예외처리 통일

---
+++ src/main/java/com/example/clothsdanawa/cart/dto/response/CartItemResponseDto.java
@@ -0,0 +1,27 @@
+package com.example.clothsdanawa.cart.dto.response;
+
+import com.example.clothsdanawa.cart.entity.CartItem;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CartItemResponseDto {
+	private final Long storeId;
+	private final String storeName;
+	private final Long productId;
+	private final String productName;
+	private final int price;
+	private final int quantity;
+
+	public static CartItemResponseDto from(CartItem cartItem) {
+		return new CartItemResponseDto(
+			cartItem.getProduct().getStore().getStoreId(),
+			cartItem.getProduct().getStore().getCompany(),
+			cartItem.getProduct().getId(),
+			cartItem.getProduct().getProductName(),
+			cartItem.getProduct().getPrice(),
+			cartItem.getQuantity()
+		);
+	}
+}
