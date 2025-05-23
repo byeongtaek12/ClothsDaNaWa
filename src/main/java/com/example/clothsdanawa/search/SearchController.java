f61a50fa224984d98dc5d7cbd3df19e3312bad80
류형철 <fbgudcjf2@gmail.com>
2025-05-23 13:21:22 +0900

    feat: keyword 로 가게,상품 검색기능 추가

--- src/main/java/com/example/clothsdanawa/search/SearchController.java
+++ src/main/java/com/example/clothsdanawa/search/SearchController.java
@@ -1,4 +1,4 @@
-package com.example.clothsdanawa.redis;
+package com.example.clothsdanawa.search;
 
 import java.util.List;
 
@@ -6,18 +6,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.clothsdanawa.redis.RedisService;
+import com.example.clothsdanawa.search.dto.SearchResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/keywords")
-public class RedisController {
+@RequestMapping("/search")
+public class SearchController {
 
 	private final RedisService redisService;
+	private final SearchService searchService;
 
 	@GetMapping
+	public ResponseEntity<List<SearchResponseDto>> keywordSearch(@RequestParam String keyword) {
+
+		redisService.incrementCount(keyword);
+		List<SearchResponseDto> searchResponseDtos = searchService.searchAll(keyword);
+
+		return new ResponseEntity<>(searchResponseDtos, HttpStatus.OK);
+	}
+
+	@GetMapping("/top")
 	public ResponseEntity<List<String>> getTop10Keywords() {
 		List<String> top10Keywords = redisService.getTop10Keywords();
 		return new ResponseEntity<>(top10Keywords, HttpStatus.OK);
