3881f2bac1f8b495e28ed293d1f106c88ee82583
류형철 <fbgudcjf2@gmail.com>
2025-05-23 18:54:13 +0900

    refactor: properties 에서 @value값 바인딩받도록 수정

--- src/main/java/com/example/clothsdanawa/redis/RedisConfig.java
+++ src/main/java/com/example/clothsdanawa/redis/RedisConfig.java
@@ -6,16 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-	@Value("localhost")
+	@Value("${spring.data.redis.host}")
 	private String host;
 
-	@Value("6379")
+	@Value("${spring.data.redis.port}")
 	private int port;
 
 	@Bean
@@ -28,7 +27,7 @@ public class RedisConfig {
 		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
 		return redisTemplate;
 	}
 }
