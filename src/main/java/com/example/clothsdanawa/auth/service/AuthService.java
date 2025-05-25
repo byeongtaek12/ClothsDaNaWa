dc74c45be4df8e4024f0510da3b8f0f9087c2439
obt <byeongtaek12@naver.com>
2025-05-25 18:15:12 +0900

    feat: AuthService클래스) 메서드 수정 및 생성
    - 회원가입할 때는 Token 반환 안 해줘도 될 것 같아서 수정
    - 로그인시 refresh Token도 같이 생성해주고 쿠키로 만들어서 header에 넣어주는 로직 추가
    - 로그아웃 메서드 생성 및 로그아웃시 refresh Token을 null로 바꿔주고 쿠키 헤더에서도 null로 바꿔줌
    - refreshToken 검증 메서드도 두개 추가

--- src/main/java/com/example/clothsdanawa/auth/service/AuthService.java
+++ src/main/java/com/example/clothsdanawa/auth/service/AuthService.java
@@ -1,17 +1,27 @@
 package com.example.clothsdanawa.auth.service;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.clothsdanawa.auth.dto.AuthLoginRequestDto;
-import com.example.clothsdanawa.auth.dto.AuthResponseDto;
+import com.example.clothsdanawa.auth.dto.AuthLoginResponseDto;
 import com.example.clothsdanawa.auth.dto.AuthSignUpRequestDto;
+import com.example.clothsdanawa.auth.dto.AuthSignupResponseDto;
 import com.example.clothsdanawa.common.exception.BaseException;
 import com.example.clothsdanawa.common.exception.ErrorCode;
 import com.example.clothsdanawa.common.jwt.JwtUtil;
+import com.example.clothsdanawa.common.security.CustomUserPrincipal;
 import com.example.clothsdanawa.user.entity.User;
 import com.example.clothsdanawa.user.repository.UserRepository;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -22,7 +32,8 @@ public class AuthService {
 	private final PasswordEncoder passwordEncoder;
 	private final JwtUtil jwtUtil;
 
-	public AuthResponseDto signup(AuthSignUpRequestDto authSignUpRequestDto) {
+	@Transactional
+	public AuthSignupResponseDto signup(AuthSignUpRequestDto authSignUpRequestDto) {
 
 		if (userRepository.existsByEmail(authSignUpRequestDto.getEmail())) {
 			throw new BaseException(ErrorCode.CONFLICT_EMAIL);
@@ -34,13 +45,11 @@ public class AuthService {
 
 		User savedUser = userRepository.save(user);
 
-		String jwtToken = jwtUtil.createToken(savedUser.getUserId(), savedUser.getName(), savedUser.getEmail(),
-			savedUser.getUserRole());
-
-		return AuthResponseDto.of(savedUser, jwtToken);
+		return AuthSignupResponseDto.of(savedUser);
 	}
 
-	public AuthResponseDto login(AuthLoginRequestDto authLoginRequestDto) {
+	@Transactional
+	public AuthLoginResponseDto login(AuthLoginRequestDto authLoginRequestDto, HttpServletResponse response) {
 
 		User findedUser = userRepository.findByEmailAndDeletedAtIsNullOrElseThrow(authLoginRequestDto.getEmail());
 		if (!passwordEncoder.matches(authLoginRequestDto.getPassword(), findedUser.getPassword())) {
@@ -49,7 +58,85 @@ public class AuthService {
 
 		String jwtToken = jwtUtil.createToken(findedUser.getUserId(), findedUser.getName(), findedUser.getEmail(),
 			findedUser.getUserRole());
+		String refreshToken = jwtUtil.createRefreshToken(findedUser.getUserId());
+		ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
+			.httpOnly(true)
+			.secure(true)
+			.sameSite("Strict")
+			.maxAge(TimeUnit.DAYS.toSeconds(7))
+			.build();
+		findedUser.updateRefreshToken(refreshToken);
+		response.addHeader("Set-Cookie", cookie.toString());
 
-		return AuthResponseDto.of(findedUser, jwtToken);
+		return AuthLoginResponseDto.of(findedUser, jwtToken);
+	}
+
+	@Transactional
+	public void logout(CustomUserPrincipal customUserPrincipal, HttpServletRequest request,
+		HttpServletResponse response) {
+
+		String refreshToken = Arrays.stream(request.getCookies())
+			.filter(a -> "refreshToken".equals(a.getName()))
+			.map(Cookie::getValue)
+			.findFirst()
+			.orElse(null);
+
+		User authUser = vaildRefreshToken(customUserPrincipal, refreshToken);
+
+		authUser.updateRefreshToken("null");
+
+		ResponseCookie cookie = ResponseCookie.from("refreshToken", null)
+			.maxAge(0)
+			.build();
+		response.addHeader("Set-Cookie", cookie.toString());
+
+	}
+
+	public AuthLoginResponseDto reissueToken(HttpServletRequest request) {
+
+		String refreshToken = Arrays.stream(request.getCookies())
+			.filter(a -> a.getName().equals("refreshToken"))
+			.map(Cookie::getValue)
+			.findFirst().orElse(null);
+		User vaildedUser = vaildRefreshToken1(refreshToken);
+
+		String token = jwtUtil.createToken(vaildedUser.getUserId(), vaildedUser.getName(), vaildedUser.getEmail(),
+			vaildedUser.getUserRole());
+
+		return AuthLoginResponseDto.of(vaildedUser, token);
+	}
+
+	private User vaildRefreshToken(CustomUserPrincipal customUserPrincipal, String refreshToken) {
+		User authUser = userRepository.findByUserIdAndDeletedAtIsNullOrElseThrow(customUserPrincipal.getUserId());
+		if (authUser.getRefreshToken() == null || refreshToken == null) {
+			throw new BaseException(ErrorCode.UNAUTHORIZED_NULL_REFRESH_TOKEN);
+		}
+		if (!authUser.getRefreshToken().equals(refreshToken)) {
+			throw new BaseException(ErrorCode.FORBIDDEN_BUT_LOGIN_OK);
+
+		}
+		if (jwtUtil.isTokenExpired(refreshToken)) {
+			throw new BaseException(ErrorCode.UNAUTHORIZED_REFRESH_TOKEN);
+		}
+		return authUser;
+	}
+
+	private User vaildRefreshToken1(String refreshToken) {
+
+		Long userId = Long.valueOf(jwtUtil.extractClaims1(refreshToken).getSubject());
+
+		User findedUser = userRepository.findByUserIdAndDeletedAtIsNullOrElseThrow(userId);
+
+		if (findedUser.getRefreshToken() == null || refreshToken == null) {
+			throw new BaseException(ErrorCode.UNAUTHORIZED_NULL_REFRESH_TOKEN);
+		}
+		if (!findedUser.getRefreshToken().equals(refreshToken)) {
+			throw new BaseException(ErrorCode.FORBIDDEN_BUT_LOGIN_OK);
+
+		}
+		if (jwtUtil.isTokenExpired(refreshToken)) {
+			throw new BaseException(ErrorCode.UNAUTHORIZED_REFRESH_TOKEN);
+		}
+		return findedUser;
 	}
 }
