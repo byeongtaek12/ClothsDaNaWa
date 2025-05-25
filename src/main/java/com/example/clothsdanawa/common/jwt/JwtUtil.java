63f919c90cb55bca9b26d74ed577cd487e019ef6
obt <byeongtaek12@naver.com>
2025-05-25 18:18:14 +0900

    feat: JwtUtil클래스) 리팩토링 및 메서드 추가
    - TimeUnit 기능으로 토큰 만료시간을 수정하여 가독성 좋게끔 했음
    - refreshToken을 만드는 메서드 추가
    - refreshToken 검증 로직 추가

--- src/main/java/com/example/clothsdanawa/common/jwt/JwtUtil.java
+++ src/main/java/com/example/clothsdanawa/common/jwt/JwtUtil.java
@@ -3,6 +3,7 @@ package com.example.clothsdanawa.common.jwt;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -20,17 +21,23 @@ import jakarta.annotation.PostConstruct;
 public class JwtUtil {
 
 	private static final String BEARER_PREFIX = "Bearer ";
-	private static final long TOKEN_TIME = 60 * 60 * 1000L;
+	private static final long TOKEN_TIME = TimeUnit.MINUTES.toMillis(1);
+	private static final long REFRESH_TOKEN_TIME = TimeUnit.DAYS.toMillis(7);
 
 	@Value("${jwt.secret.key}")
 	private String secretKey;
+	@Value("${jwt.secret.key1}")
+	private String secretKey1;
 	private Key key;
+	private Key key1;
 	private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
 	@PostConstruct
 	public void init() {
 		byte[] bytes = Base64.getDecoder().decode(secretKey);
+		byte[] bytes1 = Base64.getDecoder().decode(secretKey1);
 		key = Keys.hmacShaKeyFor(bytes);
+		key1 = Keys.hmacShaKeyFor(bytes1);
 	}
 
 	public String createToken(Long userId, String name, String email, UserRole userRole) {
@@ -47,6 +54,17 @@ public class JwtUtil {
 			.compact();
 	}
 
+	public String createRefreshToken(Long userId) {
+		Date date = new Date();
+
+		return Jwts.builder()
+			.setSubject(String.valueOf(userId))
+			.setExpiration(new Date(date.getTime() + REFRESH_TOKEN_TIME))
+			.setIssuedAt(date)
+			.signWith(key1, signatureAlgorithm)
+			.compact();
+	}
+
 	public String substringToken(String tokenValue) {
 		if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
 			return tokenValue.substring(7);
@@ -60,6 +78,25 @@ public class JwtUtil {
 			.build()
 			.parseClaimsJws(token)
 			.getBody();
+	}
+
+	public Claims extractClaims1(String token) {
+		return Jwts.parserBuilder()
+			.setSigningKey(key1)
+			.build()
+			.parseClaimsJws(token)
+			.getBody();
+	}
+
+	public boolean isTokenExpired(String refreshToken) {
+		Claims claims = Jwts.parserBuilder()
+			.setSigningKey(key1)
+			.build()
+			.parseClaimsJws(refreshToken)
+			.getBody();
+
+		Date expiration = claims.getExpiration();
+		return expiration.before(new Date());
 	}
 }
 
